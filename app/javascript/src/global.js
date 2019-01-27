b868438a945a5fadca1f7b9d21e4dba6024937bc
Peter <psmir777@gmail.com>
2019-01-27 20:26:35 +0300

    Add Landing page

--- app/javascript/src/global.js
+++ app/javascript/src/global.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-  alert('Hello!!');
+  $( window ).scroll(function() {
+    var scrollPos = $(document).scrollTop();
+    var $navbar = $('#fancy_header .navbar').first();
+    
+    if(scrollPos > 0) {
+      $navbar.removeClass('initial').addClass('bg-dark');
+    } else {
+      $navbar.addClass('initial').removeClass('bg-dark');
+    }
+  });
 });
