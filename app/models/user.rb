65fc9ff06bb04c788f6d3c3af259ee48598d10cc
Peter <psmir777@gmail.com>
2019-03-31 11:03:13 +0300

    Add roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   has_secure_password
+  enum role: %i[client consultant admin]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_09_155914) do
+ActiveRecord::Schema.define(version: 2019_03_31_075354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_03_09_155914) do
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "password_digest", null: false
+    t.integer "role", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
