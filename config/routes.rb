272c8a5e9296130037dc57af86b3f387814b7b27
Peter <psmir777@gmail.com>
2019-04-13 08:12:32 +0300

    Add Consultant Panel

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-  get :dashboard, to: "dashboard#show"
+
+  get :dashboard, to: 'dashboard#show'
+  get :consultant_panel, to: 'consultant_panel#show'
 
   resource :session
   resource :users, only: %i[new create]
-  
-  root to: "landing#index"
+
+  root to: 'landing#index'
 end
